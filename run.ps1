aa76476311ec47ae1174d7c410d6e186d4334d2d
Anthony Segedi <79811294+segedi-UW@users.noreply.github.com>
2021-05-13 13:05:21 -0500

    Added build scripts
    Added the build scripts for ease of use. If needed, the compile and run commands can be copied in bash for the same result

---
+++ run.ps1
@@ -0,0 +1,7 @@
+﻿$errorCount = $error.count
+$name = $args[0]
+$javaFxLibPath = "C:\Users\belai\Desktop\clProjects\javaFxLib\openjfx-11.0.2_windows-x64_bin-sdk\javafx-sdk-11.0.2\lib"
+javac -cp ".;.\images" --module-path $javaFxLibPath --add-modules javafx.controls,javafx.fxml "$name.java"
+if ($errorCount -eq $error.count -and $lastExitCode -eq 0) {
+    java -cp ".;.\images" --module-path $javaFxLibPath --add-modules javafx.controls,javafx.fxml $name
+}
---
+++ test.ps1
@@ -0,0 +1,6 @@
+﻿$errorCount = $error.count
+$name = "TestSuite"
+javac -cp  ".;junit5.jar" "$name.java"
+if ($error.count -eq $errorCount -and $lastExitCode -eq 0) { # The number of errors has not increased
+    java -jar junit5.jar -cp . --scan-classpath
+}
